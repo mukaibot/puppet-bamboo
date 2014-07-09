38bb34447d660cffb44f44fd119fa567c4f5e06d
Timothy Mukaibo <timothy@mukaibo.com>
2014-07-09 18:52:01 +1000

    Change installpath

--- manifests/ruby.pp
+++ manifests/ruby.pp
@@ -1,11 +1,14 @@
 # == Class bamboo::ruby
 # Installs rbenv into the bamboo user's home dir. This is to enable the ruby plugin
 class bamboo::ruby($rubyver = '2.1.2') {
+  $user = hiera('bamboo::users::username', 'bamboo')
+
   class { 'rbenv': }
   rbenv::plugin { 'sstephenson/ruby-build': }
   rbenv::build { '2.1.2':
-    owner  => hiera('bamboo::users::username', 'bamboo'),
-    group  => hiera('bamboo::users::username', 'bamboo'),
-    global => true
+    owner       => $user,
+    group       => $user,
+    global      => true,
+    install_dir => "/home/${user}"
   }
 }

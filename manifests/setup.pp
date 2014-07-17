f87271c3fe9f5c4ce7134a9081016a336c449b0d
Timothy Mukaibo <timothy@mukaibo.com>
2014-07-18 08:37:06 +1000

    Add init script for Centos / RedHat

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -7,6 +7,7 @@ class bamboo::setup {
 
   $user = $bamboo::username
   $packagename = "atlassian-bamboo-${bamboo::version}"
+  $bamboo_dir = "/home/${user}/${packagename}"
   $tarball = "${packagename}.tar.gz"
 
   exec { 'bamboo_tarball':
@@ -18,26 +19,21 @@ class bamboo::setup {
   } ->
   exec { 'extract':
     command => "/bin/tar xvzf ${tarball}",
-    creates => "/home/${user}/${packagename}",
+    creates => $bamboo_dir,
     cwd     => "/home/${user}",
     user    => $user,
-  } ->
+  }
+
   file { '/etc/init.d/bamboo':
-    ensure => link,
-    target => "/home/${user}/${packagename}/bin/start-bamboo.sh"
-  } ->
-  file { '/etc/default/bamboo':
     ensure  => present,
-    content => "RUN_AS_USER=${user}
-BAMBOO_PID=/home/${user}/bamboo.pid
-BAMBOO_LOG_FILE=/home/${user}/logs/bamboo.log",
+    content => template("bamboo/bamboo.${::osfamily}.init.erb")
   }
 
   file { 'bamboo-home':
     ensure   => 'present',
     require  => Exec['extract'],
     content  => "bamboo.home= ${bamboo::home}",
-    path     => "/home/${user}/${packagename}/atlassian-bamboo/WEB-INF/classes/bamboo-init.properties"
+    path     => "${bamboo_dir}/atlassian-bamboo/WEB-INF/classes/bamboo-init.properties"
   }
 
 }
